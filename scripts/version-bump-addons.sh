2d911401ca9d028395dbb496c22c5f3a5a00b041
Jeff Hilton <jeffhilton.code@gmail.com>
2025-02-02 17:49:38 -0700

    asdf

--- scripts/version-bump-addons.sh
+++ scripts/version-bump-addons.sh
@@ -7,4 +7,9 @@ version=$(echo $addons | jq ".version" | tr -d '"')
 patch_bump=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
 addons=$(echo $addons | jq --arg v $patch_bump '.version = $v')
 echo $addons | jq . > $file
+
+if [[ -n $GITHUB_ENV ]]; then
+  echo "NEXT_VERSION=$bumped" >> $GITHUB_ENV
+fi
+
 echo "Bumped from $version to $patch_bump"
--- scripts/version-bump-plugin.sh
+++ scripts/version-bump-plugin.sh
@@ -5,6 +5,11 @@ file=./addons/$1/plugin
 mv "$file.cfg" "$file.toml"
 version=$(dasel select -f $file.toml -r toml -s "plugin.version" | tr -d "'")
 bumped=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
+
+if [[ -n $NEXT_VERSION ]]; then
+  bumped=$NEXT_VERSION
+fi
+
 dasel put -t string -v $bumped -f "$file.toml" -r toml "plugin.version" 
 mv "$file.toml" "$file.cfg"
 
