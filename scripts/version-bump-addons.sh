c587095cdeb64d462234c22eb2322edaecd5f3f0
Jeff Hilton <jeffhilton.code@gmail.com>
2025-02-02 18:08:58 -0700

    Update version-bump-addons.sh

--- scripts/version-bump-addons.sh
+++ scripts/version-bump-addons.sh
@@ -8,8 +8,6 @@ patch_bump=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
 addons=$(echo $addons | jq --arg v $patch_bump '.version = $v')
 echo $addons | jq . > $file
 
-if [[ -n $GITHUB_OUTPUT ]]; then
-  echo "next_version=${version}" >> $GITHUB_OUTPUT
-fi
+echo "next_version=${version}" >> $GITHUB_OUTPUT
 
 echo "Bumped from $version to $patch_bump"
